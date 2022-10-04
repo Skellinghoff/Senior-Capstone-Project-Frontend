103e4d43026edabc7d445e8ff2dd4a0c113c0f56
skellinghoff <Jarodwellinghoff@gmail.com>
2022-10-03 21:15:47 -0500

    asdf

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 }
 
 function callApi() {
-    fetch('https://simple-node-server-niru.herokuapp.com/details', { method: 'GET' })
+    fetch('https://my-tech-map.herokuapp.com/', { method: 'GET' })
         .then(data => data.json())
         .then(json => alert(JSON.stringify(json)))
 }
